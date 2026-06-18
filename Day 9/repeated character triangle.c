a21c5a7b35f9383c05b7ab7bfc1fa6af0909f8a4
DivyaAgrawal-design <divyal.cse2025@glbajajgroup.org>
2026-06-18 22:04:13 +0530

    Add files via upload

---
+++ Day 9/repeated character triangle.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main()
+{
+    int i,j,a=65;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=i;j++){
+            printf("%c",a);
+        }
+        printf("\n");
+        a=a+1;
+        
+    }
+
+    return 0;
+}
