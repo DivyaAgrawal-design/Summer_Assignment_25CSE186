a8bb9156ffe29d73146d774a34dc53bea42f8952
DivyaAgrawal-design <divyal.cse2025@glbajajgroup.org>
2026-06-20 20:13:39 +0530

    Create Sum of two numbers.c

---
+++ Day 11/Sum of two numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,s;
+    printf("enter your first number");
+    scanf("%d",&a);
+    printf("enter your second number ");
+    scanf("%d",&b);
+    s=a+b;
+    printf("the sum =%d",s);
+
+    return 0;
+}
